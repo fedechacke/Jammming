ea0e70615077601104db2bd3b559e0daef3fa231
Fede <federicochackelevicius@MacBook-Pro-de-Federico.local>
2020-03-02 12:13:29 -0300

    Add add and remove functionality and playlist name change

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,42 +1,65 @@
-import React from 'react';
-import './App.css';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
+  import React from 'react';
+  import './App.css';
+  import SearchBar from '../SearchBar/SearchBar';
+  import SearchResults from '../SearchResults/SearchResults';
+  import Playlist from '../Playlist/Playlist';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {searchResults: [
-      {name: 'name 1',
-        artist: 'artist 1',
-        album: 'album 1',
-        id: 1},
-        {name: 'name 2',
-        artist: 'artist 2',
-        album: 'album 2',
-        id: 2},
-        {name: 'name 3',
-        artist: 'artist 3',
-        album: 'album 3',
-        id: 3}
-    ]}
-  }
+  class App extends React.Component {
+  constructor (props) {
+      super(props);
+      this.state = {
+        searchResults: [{name: 'name 1', artist: 'artist 1', album: 'album 1', id: 1},
+        {name: 'name 2', artist: 'artist 2', album: 'album 2', id: 2},
+        {name: 'name 3', artist: 'artist 3', album: 'album 3', id: 3}],
+        playlistName: 'Playlist Name',
+        playlistTracks: [{name: 'PlaylistName 1', artist: 'PlaylistArtist 1', album: 'PlaylistAlbum 1', id: 4},
+        {name: 'PlaylistName 2', artist: 'PlaylistArtist 2', album: 'PlaylistAlbum 2', id: 5},
+        {name: 'PlaylistName 3', artist: 'PlaylistArtist 3', album: 'PlaylistAlbum 3', id: 6}]
+      };
+      this.addTrack = this.addTrack.bind(this);
+      this.removeTrack = this.removeTrack.bind(this);
+      this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    }
 
-  render(){
-  return (
-    <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <SearchBar />
-    <div className="App-playlist">
-      <SearchResults searchResults={this.state.searchResults}/>
-      <Playlist />
+    addTrack(track){
+      let tracks = this.state.playlistTracks;
+      if(tracks.find(savedTrack => savedTrack.id === track.id)){
+        return;
+      }else{
+        tracks.push(track);
+        this.setState({playlistTracks: tracks});
+      }
+    }
+
+    removeTrack(track){
+      let tracks = this.state.playlistTracks;
+      tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+      this.setState({playlistTracks: tracks});
+      }
+
+      updatePlaylistName(name){
+        this.setState({playlistName: name});
+      }
+    
+
+    render(){
+    return (
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+    <div className="App">
+      <SearchBar /> 
+      <div className="App-playlist">
+        <SearchResults searchResults={this.state.searchResults} 
+                       onAdd={this.addTrack}/>
+        <Playlist playlistName={this.state.playlistName} 
+                  playlistTracks={this.state.playlistTracks}
+                  onRemove={this.removeTrack}
+                  onNameChange={this.updatePlaylistName}/>
+      </div>
     </div>
   </div>
-</div>
-  );
+    );
+    }
   }
-}
 
-export default App;
+  export default App;
--- jammming/src/Components/TrackList/TrackList.js
+++ jammming/src/Components/TrackList/TrackList.js
@@ -5,11 +5,16 @@ import './TrackList.css';
 class TrackList extends React.Component {
     render(){
         return (
-            <div className= "TrackList" > 
-                {this.props.tracks.map(track => {
-                    return <Track track={track}
-                    key={track.id} />
-                })}
+            <div className= "TrackList"> 
+                 {
+                this.props.tracks.map(track =>  {
+                        return <Track track={track}
+                                      key={track.id}
+                                      onAdd={this.props.onAdd}
+                                      onRemove={this.props.onRemove}
+                                      isRemoval={this.props.isRemoval}/>
+                    })
+                }
             </div>
      );
     }
